3bd16b4e1e5d53869425e4a21f75d3846f694371
ChSan <ChSan@LAPTOP-0NG5T68J>
2021-08-02 22:45:56 +0100

    Production Table Data Insert Commit
    Inserted data as records into the Production Table

---
+++ SQLQuery3_InsertDataIntoProductionTable.sql
@@ -0,0 +1,17 @@
+﻿INSERT INTO Production VALUES (1, 1, 'Composite Door');
+INSERT INTO Production VALUES (1, 2, 'Casement');
+INSERT INTO Production VALUES (1, 3, 'Reversible');
+INSERT INTO Production VALUES (2, 1, 'Reversible');
+INSERT INTO Production VALUES (2, 2, 'Reversible');
+INSERT INTO Production VALUES (3, 1, 'Composite Door');
+INSERT INTO Production VALUES (3, 2, 'Compostie Door');
+INSERT INTO Production VALUES (4, 1, 'Casement');
+INSERT INTO Production VALUES (4, 2, 'Casement');
+INSERT INTO Production VALUES (4, 3, 'Composite Door');
+INSERT INTO Production VALUES (4, 4, 'Reversible');
+INSERT INTO Production VALUES (5, 1, 'Casement');
+INSERT INTO Production VALUES (5, 2, 'Casement');
+INSERT INTO Production VALUES (6, 1, 'Composite Door');
+INSERT INTO Production VALUES (7, 1, 'Reversible');
+INSERT INTO Production VALUES (8, 1, 'Reversible');
+INSERT INTO Production VALUES (8, 1, 'Casement');
