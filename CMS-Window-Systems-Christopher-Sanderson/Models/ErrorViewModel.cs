42c6a26b4f2d9a4a0370e08da14b9ed092c1d858
ChSan <ChSan@LAPTOP-0NG5T68J>
2021-08-03 10:24:29 +0100

    ASP.NET Core MVC Commit

---
+++ CMS-Window-Systems-Christopher-Sanderson/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CMS_Window_Systems_Christopher_Sanderson.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
