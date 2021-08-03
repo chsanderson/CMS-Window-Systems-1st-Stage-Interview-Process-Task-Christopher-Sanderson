9e9b6be6dee0d7a9f0cd300fb5bbec9246672212
ChSan <ChSan@LAPTOP-0NG5T68J>
2021-08-03 01:01:01 +0100

    Creating QRTable And Querying SQL Data
    Task 1 Completed Finished QRTable Creation and Also Completed Task 2 Querying SQL Data In Descending Order.

---
+++ SQLQuery7_QueryingSQLData.sql
@@ -0,0 +1,7 @@
+﻿SELECT COUNT(case when p.ProductName = 'Composite Door' then 1 else NULL END)[DoorTotal],
+COUNT(case when p.ProductName != 'Composite Door'then 1 else NULL END)[WindowTotal],
+h.DateInProduction, MONTH(h.DateInProduction) AS [Month], Year(h.DateInProduction) AS [Year]
+FROM Production AS p, Heading AS h
+WHERE p.JobKeyID=h.JobKeyID
+GROUP BY h.DateInProduction
+ORDER BY h.DateInProduction DESC;
